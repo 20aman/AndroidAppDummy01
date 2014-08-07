d9a3fc596c03c7148c77d2969b5182b26dec1ffc
Aman <20aman@gmail.com>
2014-08-07 19:46:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/amsharma/dummy01/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.amsharma.dummy01;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    TextView mainTextView;
+    Button mainButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setting up the text on App Startup
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("This text is setup via Code!!!");
+
+        //Making button event listen
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        mainTextView.setText("Voila! Button Pressed");
+    }
+}
