327ac4d8752a3851ced4d43a54d7c4b7f5faf308
Aman <20aman@gmail.com>
2014-08-08 18:41:14 +0530

    Modified Comment

--- app/src/main/java/com/example/amsharma/dummy01/MainActivity.java
+++ app/src/main/java/com/example/amsharma/dummy01/MainActivity.java
@@ -2,19 +2,29 @@ package com.example.amsharma.dummy01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+
+public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
+    ListView mainListView;
+
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,16 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         mainEditText = (EditText) findViewById(R.id.main_edittext);
 
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
+
+        //Set this activity to react to list items being pressed
+        mainListView.setOnClickListener(this);
     }
 
     @Override
@@ -57,5 +77,14 @@ public class MainActivity extends Activity implements View.OnClickListener{
     public void onClick(View view) {
         //mainTextView.setText("Voila! Button Pressed");
         mainTextView.setText(mainEditText.getText().toString() + " says hello world.");
+
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+        Log.d("aman Android app", position + ": " + mNameList.get(position));
     }
 }
